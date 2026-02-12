d33a1c8ed9357e2d540356e9ae408b4c7e32da54
kamilaserpa <kamilardg@gmail.com>
2026-02-12 11:04:06 -0300

    Adjust grid

--- src/components/sections/dashboard/shortcuts/index.tsx
+++ src/components/sections/dashboard/shortcuts/index.tsx
@@ -6,15 +6,15 @@ import ShortcutParameters from './ShortcutParameters';
 const Shortcuts = () => {
   return (
     <Grid container spacing={2.5}>
-      <Grid item xs={6} sm={6} md={4} xl={2}>
+      <Grid item xs={6} sm={6} md={4}>
         <ShortcutWeek />
       </Grid>
 
-      <Grid item xs={6} sm={6} md={4} xl={2}>
+      <Grid item xs={6} sm={6} md={4}>
         <ShortcutTraining />
       </Grid>
 
-      <Grid item xs={12} sm={6} md={4} xl={2}>
+      <Grid item xs={12} sm={6} md={4}>
         <ShortcutParameters />
       </Grid>
     </Grid>
